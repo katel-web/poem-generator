fa838504a9b43e453c883d9ded9449f308c5a071
katel-web <katelemass@gmail.com>
2024-08-18 16:11:51 +0100

    added basic javascript function for form

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem-box", {
+    strings: "Poem here la la la la",
+    autoStart: true,
+    delay: 40,
+    cursor: null,
+  });
+}
+
+let formElement = document.querySelector("#poem-generator-form");
+formElement.addEventListener("submit", generatePoem);
